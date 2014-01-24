3fa730602bec59e083a65efb8619344e41e83892
Yoav Ben Shalom <yabashbs@gmail.com>
2014-01-24 14:02:19 -0800

    Fix Wrong path in tests

--- src/test/run_game_model.bat
+++ src/test/run_game_model.bat
@@ -1,6 +1,6 @@
-start cmd.exe /K "python server/server.py"
+start cmd.exe /K "python ../server/server.py"
 ping -n 1 127.0.0.1
-start cmd.exe /K "python client/client.py 127.0.0.1"
-start cmd.exe /K "python client/client.py 127.0.0.1"
-start cmd.exe /K "python client/client.py 127.0.0.1"
-start cmd.exe /K "python client/client.py 127.0.0.1"
+start cmd.exe /K "python ../client/client.py 127.0.0.1"
+start cmd.exe /K "python ../client/client.py 127.0.0.1"
+start cmd.exe /K "python ../client/client.py 127.0.0.1"
+start cmd.exe /K "python ../client/client.py 127.0.0.1"
