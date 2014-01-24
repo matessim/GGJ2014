c80f8aeb1ec72297280ada30efc02999fee67264
Matan Mates <matan@matan.me>
2014-01-24 11:26:18 +0200

    Server can now accept connections.

--- src/server/client.py
+++ src/server/client.py
@@ -2,8 +2,8 @@
 
 
 # Class representing a (network) client for the server
-class NetworkClient(obejct):
-	def __init__(self, client_host, client_socket):
+class NetworkClient(object):
+	def __init__(self, client_socket, client_host):
 		self._host = client_host
 		self._client_socket = client_socket
 
@@ -12,4 +12,6 @@ class NetworkClient(obejct):
 	def client_socket(self):
 		return self._client_socket
 
-
+	def __repr__(self):
+		print self._host
+		return "<Client %s:%d>" % (self._host[0], self._host[1])
--- src/server/main.py
+++ src/server/main.py
@@ -3,13 +3,34 @@
 
 import sys
 import socket
+import threading
+
+from client import *
+
+# Listen on all connections
+SERVER_IP   = '0.0.0.0'
+SERVER_PORT = 1337
+SERVER_ADDR = (SERVER_IP, SERVER_PORT)
+
+MAX_BACKLOG = 4
+
+g_connections = []
 
 def main():
-	pass
+	connection_thread = threading.Thread(target=connection_loop)
+	print "Starting server accept thread"
+	connection_thread.run()
+
 
 def connection_loop():
+	sock = socket.socket()
+	sock.bind(SERVER_ADDR)
+	sock.listen(MAX_BACKLOG)
 	while True:
-
+		host, endpoint = sock.accept()
+		new_client = NetworkClient(host, endpoint)
+		print "Accepted new client:", repr(new_client)
+		g_connections.append(new_client)
 
 if __name__ == '__main__':
 	sys.exit(main())
